efdf011a6f793c1984666d7b34bbe9f23c25284e
Oleg <olegatortor111@gmail.com>
2022-06-01 17:33:06 +0300

    start js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 "use strict"; 
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastFilmFirst = prompt("Один из последних просмотренных фильмов?", ""),
+      lastFilmFirstGrade = prompt("На сколько оцените его?", ""),
+      
+      lastFilmSecond = prompt("Один из последних просмотренных фильмов?", ""),
+      lastFilmSeconsGrade = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[lastFilmFirst] = lastFilmFirstGrade;
+personalMovieDB.movies[lastFilmSecond] = lastFilmSeconsGrade;
+
+console.log(personalMovieDB);
