bea16dd9bf7f094dd9ff2f1b6d5d69e72a9b300e
Oleg <olegatortor111@gmail.com>
2022-06-02 23:42:19 +0300

    step 2

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
+if (numberOfFilms < 10){
+    console.log('Просмотрено довольно мало фильмов');
+} else if (10 < numberOfFilms < 30) {
+    console.log('Вы классический зритель');
+} else if (numberOfFilms > 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -10,13 +20,21 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastFilmFirst = prompt("Один из последних просмотренных фильмов?", ""),
-      lastFilmFirstGrade = prompt("На сколько оцените его?", ""),
-      
-      lastFilmSecond = prompt("Один из последних просмотренных фильмов?", ""),
-      lastFilmSeconsGrade = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[lastFilmFirst] = lastFilmFirstGrade;
-personalMovieDB.movies[lastFilmSecond] = lastFilmSeconsGrade;
+for (let i = 0; i < 2; i++) {
+    const lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+          lastFilmGrade = prompt("На сколько оцените его?", "");
+    if (lastFilm == '' || lastFilm == null || lastFilm.length > 50 || lastFilmGrade == '' || lastFilmGrade == null) {
+        i--;
+        continue;
+    }
+    personalMovieDB.movies[lastFilm] = lastFilmGrade;
+}
 
 console.log(personalMovieDB);
+
+
+
+/* Задание на урок:
+
+4) Потренироваться и переписать цикл еще двумя способами*/
